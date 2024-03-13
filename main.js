c1e0629b613a6d73ea49dd7dc6a8c99dcd1b2e7b
TayyabAziz11 <159780590+TayyabAziz11@users.noreply.github.com>
2024-03-13 13:49:46 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+// Favourite Number
+var favouriteNumber = 7;
+console.log("Cristiano ronaldo jersey number is ".concat(favouriteNumber));
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Favourite Number
+
+let favouriteNumber :number = 7
+
+console.log(`Cristiano ronaldo jersey number is ${favouriteNumber}`);
